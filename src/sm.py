f707a42423715d202045c7a1b5c5a1725569763c
Geraltwolf-bit <geraltwolf@yandex.ru>
2025-10-21 13:24:07 +0000

    fixed a bug

--- src/sm.py
+++ src/sm.py
@@ -19,10 +19,11 @@ col1, col2, col3 = st.columns([1, 3, 1])
 with col1:
     st.header('Monthly trend')
     sm_month = get_montly_stockmarket_trend(raw_sm)
-    chart = alt.Chart(sm_month).mark_arc(innerRadius=45, cornerRadius=25).encode(
+    month_chart = alt.Chart(sm_month).mark_arc(innerRadius=45, cornerRadius=25).encode(
         theta = 'stockmarket_value:Q',
         color = alt.value('#29b5e8')
     )
+    st.altair_chart(month_chart)
       
 with col2:
     sm = get_yearly_stockmarket_data_for_dashboard(raw_sm)
@@ -31,6 +32,7 @@ with col2:
 with col3:
     st.header('Yearly trend')
     sm_year = get_montly_stockmarket_trend(raw_sm)
-    chart = alt.Chart(sm_year).mark_arc(innerRadius=45, cornerRadius=25).encode(
+    year_chart = alt.Chart(sm_year).mark_arc(innerRadius=45, cornerRadius=25).encode(
         theta = 'stockmarket_value:Q',
         color = alt.value("#3fe829"))
+    st.altair_chart(year_chart)
